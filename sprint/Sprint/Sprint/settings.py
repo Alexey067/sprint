86055ac0f6e70f9802fdef209dac63f56911c293
Alexey067 <115927697+Alexey067@users.noreply.github.com>
2023-05-08 14:31:04 +0300

    Подключение к БД

--- sprint/Sprint/Sprint/settings.py
+++ sprint/Sprint/Sprint/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Sprint.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sprint',
+        'USER': os.getenv('FSTR_DB_LOGIN'),
+        'PASSWORD': os.getenv('FSTR_DB_PASS'),
+        'HOST': os.getenv('FSTR_DB_HOST'),
+        'PORT': os.getenv('FSTR_DB_PORT'),
     }
 }
 
