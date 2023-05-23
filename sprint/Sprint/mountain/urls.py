f61579129a540ba8f943a664958d432cce2e21dc
Alexey067 <115927697+Alexey067@users.noreply.github.com>
2023-05-23 11:45:14 +0300

    Добавление трех методов REST API

--- sprint/Sprint/mountain/urls.py
+++ sprint/Sprint/mountain/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('submit-data/', views.submitData, name='submit-data'),
+    path('submitData/<int:pk>/', views.get_pereval_by_id, name='pereval-detail'),
+    path('submitData/<int:pk>/update/', views.edit_pereval, name='pereval-update'),
+    path('submitData/email/', views.get_pereval_by_user_email, name='pereval-list'),
 ]
